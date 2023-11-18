c39a9629fb392fd8bbfc1520a87139fe4651cb11
Fábio Vasconcelos <fabio.vasconcelos@fabamaq.com>
2023-11-18 15:48:58 +0000

    Coffee MAchine exercise done.

--- main.py
+++ main.py
@@ -1,17 +1,91 @@
+import utils
 
-# This is a sample Python script.
+isOn = True
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+water = utils.resources["water"]
+milk = utils.resources["milk"]
+coffee = utils.resources["coffee"]
+money = 10
+
+def GetResourceValue (product, resource):
+    if resource in utils.MENU[product]["ingredients"]:
+        return utils.MENU[product]["ingredients"][resource]
+    else:
+        return 0
+def GetResourceAvailability (product, resource):
+    if resource in utils.MENU[product]["ingredients"]:
+        return utils.MENU[product]["ingredients"][resource] <= utils.resources[resource]
+    return True
+
+def GetWaterAvailability(product):
+    return GetResourceAvailability(product, "water")
+def GetCoffeeAvailability(product):
+    return GetResourceAvailability(product, "coffee")
+def GetMilkAvailability(product):
+    return GetResourceAvailability(product, "milk")
+
+def GetAvailability(product):
+    if product == "espresso" or product == "latte" or product == "cappuccino":
+        if not GetWaterAvailability(product):
+            print("Sorry there is not enough water.")
+            return False
+        if not GetCoffeeAvailability(product):
+            print("Sorry there is not enough coffee.")
+            return False
+        if not GetMilkAvailability(product):
+            print("Sorry there is not enough milk.")
+            return False
+    else:
+        print("Product is not valid")
+        return False
+
+    return True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def ReadCoins():
+    quarters = int(input("Insert quarters: "))
+    dimes = int(input("Insert dimes: "))
+    nickles = int(input("Insert nickles: "))
+    pennies = int(input("Insert pennies: "))
+
+    return quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def isEnoughMoney(valueInserted, product):
+    result = valueInserted >= utils.MENU[product]["cost"]
+    if not result:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
+    return True
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+while isOn:
+    request = input("What would you like? (espresso/latte/cappuccino):")
+
+    if request == "off":
+        isOn = False
+        break
+
+    if request == "report":
+        print(f"Water: {water} ml"
+              f"Milk: {milk}ml"
+              f"Coffee: {coffee}g"
+              f"Money: ${money}")
+
+    else:
+        available = GetAvailability(request)
+
+        if available:
+            valueInserted = ReadCoins()
+            transactionStatus = isEnoughMoney(valueInserted, request)
+            if transactionStatus:
+                exchange = valueInserted - utils.MENU[request]["cost"]
+                money += utils.MENU[request]["cost"]
+                water -= GetResourceValue(request, "water")
+                milk -= GetResourceValue(request, "milk")
+                coffee -= GetResourceValue(request, "coffee")
+
+                if exchange > 0:
+                    print(f"Here is ${exchange} dollars in change.")
+
+                print(f"Here is your {request}. Enjoy!")
